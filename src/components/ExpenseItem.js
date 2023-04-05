e9b211737566d741d0fde4c928131f0c0ea1a0f8
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-05 15:46:18 +0430

    Splitting Components Into Multiple Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,12 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
 
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      {/* <ExpenseDate></ExpenseDate> */}
+       <ExpenseDate date = {props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
@@ -23,4 +17,4 @@ function ExpenseItem(props) {
 
 export default ExpenseItem;
 
-const x = new Date().toLocaleString
+// const x = new Date().toLocaleString
