d6e48d269b6798d3f341691c685f769e95d23aed
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-05 14:50:58 +0430

    add some js  logic to component

--- src/App.js
+++ src/App.js
@@ -51,3 +51,5 @@ function App() {
 }
 
 export default App;
+
+
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
@@ -13,3 +22,5 @@ function ExpenseItem(props) {
 }
 
 export default ExpenseItem;
+
+const x = new Date().toLocaleString
