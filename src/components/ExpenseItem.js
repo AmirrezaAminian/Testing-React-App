2e4e8782bb7e6cf8404efcfcafb3560580e658bf
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-07 03:40:09 +0430

    children props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,17 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
   return (
-    <div className="expense-item">
-      {/* <ExpenseDate></ExpenseDate> */}
-       <ExpenseDate date = {props.date}/>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
 export default ExpenseItem;
-
-// const x = new Date().toLocaleString
