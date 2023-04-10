99a668eedadd2767da822dfe5e0285316bc8ca9b
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-11 03:15:11 +0430

    add some conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
@@ -16,17 +17,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-   let expenseContent = <p>No expenses found.</p>
-   if(filteredExpenses.length > 0 ){
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-   }
+  
   return (
     <div>
       <Card className="expenses">
@@ -34,7 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expenseContent }
+        <ExpensesList items = {filteredExpenses}/>
       </Card>
     </div>
   );
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import 'ExpensesList.css'
+import "./ExpensesList.css";
 
-const ExpensesList = props => {
+import ExpenseItem from "./ExpenseItem";
 
-}
+const ExpensesList = (props) => {
+  let expenseContent = <p>No expenses found.</p>;
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
+  }
+  
+
+  return <ul className="expenses-list">
+     {
+        props.items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+      ))}
+  </ul>
+};
 
 export default ExpensesList;
