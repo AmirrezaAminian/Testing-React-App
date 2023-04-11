2041f105d46f3d68aab2c0460875d7c03d8361c4
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-04-11 06:47:05 +0430

    styled component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -22,7 +22,9 @@ const CourseInput = (props) => {
     }
     props.onAddGoal(enteredValue);
   };
-
+  
+ 
+   
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
